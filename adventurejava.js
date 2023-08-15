b64997a09ab9143a6c0944fa57bc20b807a794e0
Riekhesh <134285472+Riekhesh@users.noreply.github.com>
2023-08-15 16:08:35 +0800

    Update adventurejava.js

--- adventurejava.js
+++ adventurejava.js
@@ -1,94 +1,106 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let next = document.getElementById("endbutton");
-  let intro = document.getElementById("intro");
-  let q1 = document.getElementById("question1");
-  let q2 = document.getElementById("question2");
-  let q3 = document.getElementById("question3");
-  let q4 = document.getElementById("question4");
-  let q5 = document.getElementById("question5");
-  let q6 = document.getElementById("question6");
-  let q7 = document.getElementById("question7");
-  let q8 = document.getElementById("question8");
+  let next = document.getElementById("nextbutton");
   let last = document.getElementById("last");
   let restart = document.getElementById("resetbutton");
-  let arrowleft = document.getElementById("<")
+  let arrowleft = document.getElementById("leftkey")
   let pagenum = document.getElementById("pagenum")
+  let startbutton = document.getElementById("startbutton")
 
 
-  let pagenumber = 1;
+  $('.questions').hide();
+  $(".buttons").hide()
+  $("#title1").hide()
+  $(".card").hide()
 
+  let start = () => {
+    $("*").css("background-color", "#1f1f1f")
+    $("*").not(".card").css("background-image", "none");
+    $('#landing').hide()
+    $('#question0').fadeIn(1000)
+    $('.buttons').show()
+    $(".card").fadeIn()
+    $('#title1').fadeIn();
+  }
+  startbutton.addEventListener("click", start)
+
+  let pagenumber = 0;
+  let factor1 = true
+  
+  // next button
   let nextbutton = () => {
-    if (pagenumber ===0) {
-      intro.style.display = "block";
-      q1.style.display = 'none';
-      pagenum.innerHTML = pagenumber
-    } else if (pagenumber === 1) {
-      $("#intro").fadeOut("fast");
-      q1.style.display = 'block';
-      pagenum.innerHTML = pagenumber
-    } else if (pagenumber === 2) {
-      q1.style.display = "none";
-      q2.style.display = 'block';
-      pagenum.innerHTML = pagenumber
-    } else if (pagenumber === 3) {
-      q2.style.display = "none";
-      q3.style.display = 'block';
-      pagenum.innerHTML = pagenumber
-    } else if (pagenumber === 4) {
-      q3.style.display = "none";
-      q4.style.display = 'block';
-      pagenum.innerHTML = pagenumber
-    } else if (pagenumber === 5) {
-      q4.style.display = "none";
-      q5.style.display = 'block';
-      pagenum.innerHTML = pagenumber
-    } else if (pagenumber === 6) {
-      q5.style.display = "none";
-      q6.style.display = 'block';
-      pagenum.innerHTML = pagenumber
-    } else if (pagenumber === 7) {
-      q6.style.display = "none";
-      q7.style.display = 'block';
-      pagenum.innerHTML = pagenumber
-    } else if (pagenumber === 8) {
-      q7.style.display = "none";
-      q8.style.display = 'block';
-      pagenum.innerHTML = pagenumber
-    }else if (pagenumber === 9) {
-      q8.style.display = "none";
-      last.style.display = 'block';
-      restart.style.display = 'block'
-      next.style.display = "none"
-      
+    if (pagenumber == 1 && $("#q1").val() == "") {
+      alert("Please do not leave it blank")
+    } else if (pagenumber == 2 && !$('input[name=q2]:checked').val()) {
+      alert("Please do not leave it blank")
+    } else if (pagenumber == 3 && $("#q3").val() == "") {
+      alert("Please do not leave it blank")
+    } else if (pagenumber ==3 && $('#q3').val() == "echo" && factor1 == true) {
+      $('.questions').hide()
+      $('#altquestion1').show()
+      // runs only once
+      factor1 = false
+    } else if (pagenumber == 3 && $('#q3').val() != "echo" && factor1 == true) {
+      $('.questions').hide()
+      $('#altquestion2').show()
+      // runs only once
+      factor1 = false
+    } else if (pagenumber == 4 && !$('input[name=q4]:checked').val()) {
+      alert("Please do not leave it blank")
+    } else if (pagenumber == 5 && !$('input[name=q5]:checked').val()) {
+      alert("Please do not leave it blank")
+    } else if (pagenumber == 5 && $('#question5:checked').val() == "moon" && factor1 == true ) {
+      alert("it worked")
+    } else if (pagenumber == 6 && !$('input[name=q6]:checked').val()) {
+      alert("Please do not leave it blank")
+    } else if (pagenumber == 7 && !$('input[name=q7]:checked').val()) {
+      alert("Please do not leave it blank")
+    } else if (pagenumber == 8 && !$('input[name=q8]:checked').val()) {
+      alert("Please do not leave it blank")
+    } else if (pagenumber == 9) {
+      $(".buttons").hide()
+      next.removeEventListener("click", nextbutton);
+    } else {
+    pagenumber++; 
+    pagenum.innerHTML = pagenumber
+    $('#question0').hide();
+    $('.questions').hide();
+    $('#question' + pagenumber).fadeIn();
+    
     }
-    pagenumber++;
-    console.log(`page number : ${pagenumber}`);
   };
 
   next.addEventListener("click", nextbutton);
 
+  //landing page
+
+
+
+
+  // left arrow key button
+  let arrowleftkey = ()=> {
+    //preventing pagenumber from being negative
+    if (pagenumber > 0) {
+      pagenumber--;
+    } else {
+      pagenumber * 1
+    }
+    pagenum.innerHTML = pagenumber
+    $('#question0').hide();
+    $('.questions').hide();
+    $('#question' + pagenumber).show();
+  }
+  
+  arrowleft.addEventListener("click", arrowleftkey, );
+
+  // restart button
   let reset = ()=> {
     location.reload()
   }
 
   restart.addEventListener("click", reset)
 
-  let handlePageVisibility = ()=> {
-    if (pagenumber === 2) {
-      $("div:not(#question1, #end)").hide()
-
-    }
-  }
-
-  let arrowleftkey = ()=> {
-    pagenumber--
-    pagenumber--
-    
-    nextbutton()
-    handlePageVisibility()
-  }
+  $(".card").click(function(){
+    $("#cardinfo").slideToggle(1000);
+});
   
-  
-  arrowleft.addEventListener("click", arrowleftkey, );
-
 });
